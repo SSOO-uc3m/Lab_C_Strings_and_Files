7427b5355488c8bbc3b28194f767c6c5320fd49a
Carlos Tessier <cftessier@inf.uc3m.es>
2021-02-16 20:22:14 +0100

    solutions

--- Ficheros/src/ficheros.c
+++ Ficheros/src/ficheros.c
@@ -12,31 +12,157 @@
 #define LINES		75
 
 int * readNumbersFile(char * fileName, int * size){
+    int file, nread;
+    int number;
+    int * numbers = NULL;
+    int i = 0;
+    int arraySize = 0;
     
-    return NULL;
+    file = open(fileName, O_RDONLY);
+    if ( file  < 0 )
+		return(NULL); /* cannot create file */
+	
+	/*lectura de name1 BUFSIZE caracteres de una vez*/
+	while( (nread = read(file, &number, sizeof(number)) ) > 0 )
+	{
+        arraySize = (i+1) * sizeof(int); 
+		//printf("%d\n",number);
+        numbers = (int *)realloc(numbers, arraySize);
+        if(numbers == NULL){
+            perror("Error realloc");
+            *size = 0;
+            return NULL;
+            }
+        numbers[i] = number;
+        //printf("%d %d\n",numbers[i],arraySize);
+        i++;
+	}
+	if (nread==-1)
+		return (NULL); 	/* error al leer */
+	close(file);    
+    *size = i;
+    return numbers;
 }
 
 int writeNumbersFile(char * fileName, int number[], int size){
+    int file, sz;
+    file = open(fileName, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if ( file  < 0 )
+		return(-1); /* cannot create file */
         
-    return -1;
+	for(int i=0; i < size; i++)
+	{
+        //printf("%d\n",number[i]);
+		sz = write(file, &number[i], sizeof(number[i])); 
+        if (sz < 0){
+            perror("Error writing file");
+            close(file);
+            return(-2);
+            }
+	}
+	
+	close(file);
+    
+    return 0;
 }
 
 
 
 int countFileLines(char * fileName){
+    int file, nread;
+    int lines = 0;
+	char buffer [LINES];
+	if ( (file = open(fileName, O_RDONLY) ) < 0 ){
+        perror("cannot open file");
+		return(-1); /* no puede abrirse */
+    
+    }
+	/*lectura de name1 BUFSIZE caracteres de una vez*/
+	while( (nread = read(file, buffer, LINES) ) > 0 )
+	{
+		printf("%s",buffer);
+        lines+=charOccurrences(buffer,'\n');
+	}
+	if (nread==-1){
+        perror("Error reading");
+		return (-4); 	/* error  */
+        }
+        
+	close(file);
     
     
-    return -1;
+    return lines;
 }
 
+/*
+int countFileLines(char * fileName){
+    int file, nread;
+    int lines = 0;
+	char character;
+	if ( (file = open(fileName, O_RDONLY) ) < 0 ){
+        perror("cannot open file");
+		return(-1); // no puede abrirse 
+    
+    }
+	
+	//lectura de name1 BUFSIZE caracteres de una vez
+	while( (nread = read(file, &character, sizeof(char)) ) > 0 )
+	{
+		if (character == '\n')
+            lines++;
+	}
+	if (nread==-1){
+        perror("Error reading");
+		return (-4); 	// error  
+        }
+    
+	close(file);    
+    
+    return lines;
+}*/
 
 int countFileWords(char * fileName){
+    int file, nread;
+    int words = 0;
+	char buffer [BUFSIZE];
+	if ( (file = open(fileName, O_RDONLY) ) < 0 ){
+        perror("cannot open file");
+		return(-1); /* no puede abrirse */
     
-    return -1;;
+    }
+	/*lectura de name1 BUFSIZE caracteres de una vez*/
+	while( (nread = read(file, buffer, BUFSIZE) ) > 0 )
+	{
+		printf("%s",buffer);
+        words+=countWords(buffer);
+	}
+	if (nread==-1){
+        perror("Error reading");
+		return (-4); 	/* error  */
+        }
+        
+	close(file);
+    
+    
+    return words;
 }
 
 
 int readFile(char * fileName){
+    int file, nread;
+	char buffer [BUFSIZE];
+	if ( (file = open(fileName, O_RDONLY) ) < 0 )
+		return(-1); /* no puede abrirse */
+	
+	/*lectura de name1 BUFSIZE caracteres de una vez*/
+	while( (nread = read(file, buffer, BUFSIZE) ) > 0 )
+	{
+		printf("%s",buffer);
+	}
+	if (nread==-1)
+		return (-4); 	/* error al leer */
+	close(file);
     
-   return -1;
+    return 0;
+
 }
--- Utilidades/src/utilidades.c
+++ Utilidades/src/utilidades.c
@@ -5,58 +5,182 @@
 #include <stdlib.h>
 
 int charOccurrences (char * string, char character){
-    
-    return -1;
+    int occurrences = 0;
+	for(int i = 0; i<  strlen(string); i++){
+        if (string[i]==character)
+            occurrences ++;
+        } 	
+
+	return occurrences;
 }
 
 char * copyString (char * string){
-	
+	char * sCopy;
+ 	sCopy = malloc(sizeof(char) * strlen(string));
+	strcpy(sCopy,string);
 
-	return NULL;
+	return sCopy;
 }
 
 
 
 char * upperCase(char * string){
-	
+	char * sUpperCase;
+	int i = 0;
 
-	return NULL;
+	sUpperCase = copyString(string);
+
+	while(sUpperCase[i])
+  	{
+                   sUpperCase[i] = (char)toupper(sUpperCase[i]);
+                    i++;
+  	}
+
+	return sUpperCase;
 
 }
 
 char * lowerCase(char * string){
 
-	
-	return NULL;
+	char * sLowerCase;
+	int i = 0;
+
+	sLowerCase = copyString(string);
+
+	while(sLowerCase[i])
+  	{
+                 sLowerCase[i] = (char)tolower(sLowerCase[i]);
+                 i++;
+  	}
+
+	return sLowerCase;
 
 }
 
 char * palindrome(char * string){
-	
+	char * sPalindrome;
 
-	return NULL;
+	sPalindrome = copyString(string);
+
+	int i = 0, j = 0;
+	char temp;
+              j = strlen(sPalindrome) - 1;
+ 
+   	while (i < j) {
+                 temp = sPalindrome[i];
+                 sPalindrome[i] = sPalindrome[j];
+                sPalindrome[j] = temp;
+                 i++;
+                 j--;
+
+   	}
+
+	return sPalindrome;
 }
 
-int countVowels(char * string){     
+int countVowels(char * string){
+     
+    char *pt;    
+    int vowels = 0;
+    //assign address of str1 to pt
+    pt=string;
+     
    
-    return -1;
+    while (*pt)
+    {
+        if(*pt=='A' ||*pt=='E' ||*pt=='I' ||*pt=='O' ||*pt=='U' ||*pt=='a' ||*pt=='e' ||*pt=='i' ||*pt=='o' ||*pt=='u'){
+            vowels++;
+        }
+        pt++; //pointer is increasing for searching the next character
+    }     
+       
+    return vowels;
 }
     
-int countWords(char * string){  
-    
-    return -1;
+int countWords(char * string){
+  
+   int state = 0;  
+   char * str = string;
+   unsigned wc = 0; // word count  
+  
+    // Scan all characters one by one  
+    while (*str)  
+    {  
+        // If next character is a separator, set the  
+        // state as OUT  
+        if (*str == ' ' || *str == '\n' || *str == '\t')  
+            state = 0;  
+  
+        // If next character is not a word separator and  
+        // state is OUT, then set the state as IN and  
+        // increment word count  
+        else if (state == 0)  
+        {  
+            state = 1;  
+            ++wc;  
+        }  
+  
+        // Move to next character  
+        ++str;  
+    }  
+  
+    return wc; 
 }
 
 char ** getWords(char * string){
-        
-    return NULL;
   
+  char * token;
+  char * delim = " ,.-()";
+  char ** words;
+  int i = 0;
+  int size = countWords(string);
+   char copy[strlen(string)];
+   strcpy(copy, string);
+  //printf("getWords %d\n",size);
+   words =  malloc (size* sizeof(char*));
+   if (words == NULL){
+        perror("Error");    
+        return NULL;
+   }
+   
+
+  // divide un string en una serie de tokens usando el delimitador delim.
+  token = strtok (copy,delim);
+  while (token != NULL)
+  {
+    size = strlen(token);
+    words[i] = malloc ( size* sizeof(char*));
+    if (words[i] == NULL){
+        perror("Error");
+        //free(words);
+        return NULL;
+    }
+        
+    strcpy(words[i],token);
+    //printf("getwords %s %d\n",words[i],size);
+    
+    token = strtok (NULL, delim);
+    i++;
+  }
+ 
+ return words;
 }
    
 
 
 void writeWords(char * string){
-  
+  char * token;
+  char * delim = " ,.-()";
+  char copy[strlen(string)];
+   strcpy(copy, string);
+  // divide un string en una serie de tokens usando el delimitador delim.
+  token = strtok (copy,delim);
+  printf("Words: \n");
+  while (token != NULL)
+  {
+    printf ("%s\n",token);
+    token = strtok (NULL, delim);
+  }
+
 	
 }
-
